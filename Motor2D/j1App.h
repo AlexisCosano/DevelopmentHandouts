c12a33578ad3009a5b95bd68850b16256e8ac629
Alexis Cosano Rodríguez <Alcoro13@gmail.com>
2018-09-20 14:29:34 +0200

    - TODO 1 done - TODO 2 done - TODO 3 done

--- Motor2D/j1App.h
+++ Motor2D/j1App.h
@@ -76,9 +76,9 @@ private:
 	uint				frames;
 	float				dt;
 
-	// TODO 2: Create two new variables from pugui namespace:
-	// a xml_document to store the while config file and
-	// a xml_node to read specific branches of the xml
+	// TODO 2: Done
+	pugi::xml_document config_document;
+	pugi::xml_node config_node;
 
 	int					argc;
 	char**				args;
